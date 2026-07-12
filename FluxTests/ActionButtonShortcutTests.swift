11845928cc8015a4a2d81f8e1d9cab394732a247
Wing <wing.ws.lee@gmail.com>
2026-07-12 17:03:27 +0800

    chore: bump app version to 1.11

--- FluxTests/ActionButtonShortcutTests.swift
+++ FluxTests/ActionButtonShortcutTests.swift
@@ -71,8 +71,8 @@ final class ActionButtonShortcutTests: XCTestCase {
     func testControlWidgetExtensionVersionMatchesContainingApp() throws {
         let project = try sourceContents(at: "Flux.xcodeproj/project.pbxproj")
 
-        XCTAssertEqual(project.components(separatedBy: "CURRENT_PROJECT_VERSION = 70;").count - 1, 4)
-        XCTAssertEqual(project.components(separatedBy: "MARKETING_VERSION = 1.10;").count - 1, 4)
+        XCTAssertEqual(project.components(separatedBy: "CURRENT_PROJECT_VERSION = 71;").count - 1, 4)
+        XCTAssertEqual(project.components(separatedBy: "MARKETING_VERSION = 1.11;").count - 1, 4)
     }
 
     func testShortcutCopyUsesDailySumBranding() throws {
